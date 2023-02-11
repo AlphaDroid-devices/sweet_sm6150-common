bd7acd852ac359792a221ce58aab325104192599
chuspita <kosecki99@gmail.com>
2023-02-11 08:18:17 +0100

    sm6150-common: Rename overlay

--- sm6150.mk
+++ sm6150.mk
@@ -301,7 +301,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-blaze
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
